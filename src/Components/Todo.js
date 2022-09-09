802fad4339acda425549d719537646ab0b816c8e
codersufiyan <sufiyanandkhan@gmail.com>
2022-09-09 22:48:38 +0530

    added delete functionality

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -4,26 +4,39 @@ export default class Todo extends Component {
     constructor() {
         super();
         this.state = {
-            tasks: [{id: 1, task: 'play cricket'}, {id: 2, task: 'do HW'}]
-            ,
+            tasks: [{id: 1, task: 'play cricket'}, {id: 2, task: 'do HW'}],
             currTask: ""
         } 
         
     }
-
     handleChange = (e)=>{
-        console.log(e.target.value);
+        // console.log(e.target.value);
         this.setState({
-            currTask: e.target.value
+            currTask: e.target.value  
+        })
+    }
+    handleSubmit = (e)=>{
+        // console.log(e.target.value);
+        this.setState({
+            tasks: [...this.state.tasks, {task: this.state.currTask, id: this.state.tasks.length+1}]
+        })  
+    }
+    handleDelete = (id)=>{
+        let newArr = this.state.tasks.filter((taskObj)=>{
+            return taskObj.id !== id;
+        });
+        this.setState({
+            tasks: [...newArr]
         })
     }
   render() {
     console.log('render() called');
+    // let _this = this;
     return (
         // jsx starts here
       <div>
         <input type="text" value={this.state.currTask} onChange={this.handleChange}></input>
-        <button>Sumbit</button>
+        <button onClick={this.handleSubmit}>Submit</button>
         {
             // use when need to write JS in jsx
 
@@ -31,10 +44,10 @@ export default class Todo extends Component {
                 return (
                     <li key = {taskObj.id}>
                         <p>{taskObj.task}</p>
-                        <button>Delete</button>
+                        <button onClick={()=> this.handleDelete(taskObj.id)}>Delete</button>
                     </li>
                 )
-            })
+            }.bind(this))
         }
       </div>
     )
